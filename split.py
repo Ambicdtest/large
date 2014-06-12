1cf0467e7482c97493b5f7643409961e0d8add1a
Paweł Redzyński <pawelredzynski@gmail.com>
2014-06-12 03:02:58 +0000

    Document changes in comment

--- split.py
+++ split.py
@@ -27,4 +27,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:909745136151734615495481092688564
+# Comment to update:9097451361517346154954810926885643
