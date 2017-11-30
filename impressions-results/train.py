7b9e2648a3ceeb6fc379daf1fc2079084d9546cc
Amanda Simon <riverajacob@brennan.net>
2017-11-30 05:15:05 +0000

    Fix train

--- impressions-results/train.py
+++ impressions-results/train.py
@@ -42,4 +42,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:27005897385411324498841067457
+# Comment to update:270058973854113244988410674577
