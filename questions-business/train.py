fadcc92acd1d2ac60feeef1b02d1cb5c3e380509
Amy Hicks <lance21@mayer.com>
2018-04-21 00:10:09 +0000

    Update train

--- questions-business/train.py
+++ questions-business/train.py
@@ -42,4 +42,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:699465715728271107685429816366
+# Comment to update:6994657157282711076854298163665
