485dd953f70a78f4af98e6ff6adf7f0f95cc1899
Alexander Schepanovski <suor.web@gmail.com>
2018-05-06 16:46:25 +0000

    Fix a typo in split.py

--- events-extensions/split.py
+++ events-extensions/split.py
@@ -27,4 +27,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:998174509511973308496815
+# Comment to update:9981745095119733084968150
