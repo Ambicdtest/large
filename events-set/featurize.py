c751eb9072b0a2e65ae27421b44abb5ac5307421
Serge Kryukov <serge.krukov@gmail.com>
2018-05-24 13:17:27 +0000

    Fix comment

--- events-set/featurize.py
+++ events-set/featurize.py
@@ -22,4 +22,4 @@ with open(sys.argv[2], "w") as feat:
     data_size = os.stat(sys.argv[1]).st_size
     json.dump({"params": params_values, "work": [math.log(data_size + 1), VERSION]}, feat)
 
-# Comment to update:2707113263977524083751502812
+# Comment to update:27071132639775240837515028127
