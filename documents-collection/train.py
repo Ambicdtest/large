450739449d63da738383dbb172d9438affb779e6
Peter Rowlands (변기호) <peter@pmrowla.com>
2018-09-07 16:57:20 +0000

    Fix a typo

--- documents-collection/train.py
+++ documents-collection/train.py
@@ -23,4 +23,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:3264756387422864996053324177
+# Comment to update:32647563874228649960533241775
