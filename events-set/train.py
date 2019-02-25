188077651f8f93aa546a613af36a397f419c8e75
Maxim Shmakov <mvshmakov@mail.ru>
2019-02-25 08:44:24 +0000

    Fix train

--- events-set/train.py
+++ events-set/train.py
@@ -42,4 +42,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:139670997340633445199857241812
+# Comment to update:1396709973406334451998572418126
