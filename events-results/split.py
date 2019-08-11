3be3be608602287a7ab123e57e08f94ab6cbfdac
Aaron Maynard <laura96@gmail.com>
2019-08-11 14:22:08 +0000

    Fix a typo in split.py

--- events-results/split.py
+++ events-results/split.py
@@ -27,4 +27,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:7441492522983692220531
+# Comment to update:74414925229836922205310
