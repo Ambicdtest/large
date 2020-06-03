62ab1e1bb93e5d1174e4a8f4b92f8adcab6c166d
Maxim Shmakov <mvshmakov@mail.ru>
2020-06-03 10:56:10 +0000

    Document changes in split.py

--- phrases-dev/split.py
+++ phrases-dev/split.py
@@ -27,4 +27,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:807670217418383907935797136607
+# Comment to update:8076702174183839079357971366076
