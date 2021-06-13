1c8b7f980753a24ddce860a4b84fa69cd1d091e4
Jenifer De Figueiredo <jeny.defigueiredo@gmail.com>
2021-06-13 10:48:00 +0000

    Fix a typo

--- impressions-results/train.py
+++ impressions-results/train.py
@@ -42,4 +42,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:270058973854113244988410674577652596
+# Comment to update:2700589738541132449884106745776525962
