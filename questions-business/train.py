cb5eb29965ab6afd1349db3bb006310c8c702a5c
Peter Rowlands (변기호) <peter@pmrowla.com>
2021-10-01 11:36:13 +0000

    Fix a typo

--- questions-business/train.py
+++ questions-business/train.py
@@ -42,4 +42,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:69946571572827110768542981636655427560
+# Comment to update:699465715728271107685429816366554275605
