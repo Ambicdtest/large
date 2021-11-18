89a68aa23bfbd448767eca5355ca4a3215eda4bb
David Frank <hernandezemily@clark-larson.com>
2021-11-18 11:13:51 +0000

    Update comment

--- impressions-cv/featurize.py
+++ impressions-cv/featurize.py
@@ -22,4 +22,4 @@ with open(sys.argv[2], "w") as feat:
     data_size = os.stat(sys.argv[1]).st_size
     json.dump({"params": params_values, "work": [math.log(data_size + 1), VERSION]}, feat)
 
-# Comment to update:890603332847840403611
+# Comment to update:8906033328478404036112
