564ce31a31d4a85e74221733879715530971ea3d
Jeremy Estes <brittneycharles@gmail.com>
2022-01-03 19:09:35 +0000

    Fix a typo in split

--- events-set/split.py
+++ events-set/split.py
@@ -27,4 +27,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:388264382702295706190715700093
+# Comment to update:3882643827022957061907157000938
