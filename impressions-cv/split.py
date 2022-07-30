88f39acd248677ad2c3ff279d1d2230d582f63ab
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2022-07-30 05:48:48 +0000

    Fix comment

--- impressions-cv/split.py
+++ impressions-cv/split.py
@@ -27,4 +27,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:40034566327712022030064
+# Comment to update:400345663277120220300642
