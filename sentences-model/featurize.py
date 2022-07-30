5e4c717aded81c12da44487f16719d4c44cd264a
Maxim Shmakov <mvshmakov@mail.ru>
2022-07-30 06:51:21 +0000

    Fix a typo in featurize.py

--- sentences-model/featurize.py
+++ sentences-model/featurize.py
@@ -22,4 +22,4 @@ with open(sys.argv[2], "w") as feat:
     data_size = os.stat(sys.argv[1]).st_size
     json.dump({"params": params_values, "work": [math.log(data_size + 1), VERSION]}, feat)
 
-# Comment to update:9763523979815975715876651
+# Comment to update:97635239798159757158766512
